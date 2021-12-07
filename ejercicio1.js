991aa59515d113a3d4521db7cbaef6a82a90ac98
chris <christianbrusa@hotmail.com>
2021-12-06 23:30:15 -0300

    actualizacionm1

--- ejercicio1.js
+++ ejercicio1.js
@@ -3,18 +3,21 @@
  * Ejemplo capitalizeFirstLetter("hola") => capitalizeFirstLetter("Hola")
  * Tip: Un caracter entiende toUpperCase(https://www.w3schools.com/jsref/jsref_touppercase.asp)
  */
-function capitalizeFirstLetter(palabra) {
-    
-    
+function capitalizeFirstLetter(palabra) { 
     const [p, ...r] = palabra;
-
-    //console.log(primero);
-
+    if(palabra.length == 0){
+    	return palabra;
+    }
     p1 = p.toUpperCase();
-    return p1+r.join('');
-
+	return p1+r.join('');
 }
 
+//p es la primera letra de la palabra
+//...r es lo que resta de la palabra (spread operator)
+
+//despues se crea una variable que es p "pasado a mayuscula"
+
+//por ultimo se devuelve ambas partes juntas. 
 
 
 module.exports = capitalizeFirstLetter;
--- ejercicio2.js
+++ ejercicio2.js
@@ -2,11 +2,8 @@
  * Dado un valor saber si es par
  * 
  */
-function esPar(x) {
-    if(x % 2 == 0)
-    {
-    	return x;
-    }
+function esPar(numero) {
+    return numero % 2 == 0
 }
 
 
@@ -14,24 +11,41 @@ function esPar(x) {
  * Dado una lista de elementos saber si todos los elementos excepto el primero son pares
  * 
  */
-function laColaEsPar(lista) 
+
+
+/*function laColaEsPar(lista) 
 {
     const [p, ...r] = lista;
 
-    for(i=0;i<r.length;i++)
+    for(i=0;i<=r.length;i++)
     {
         if(!esPar(r[i]))
         {
             return false;
         }
-        else
-        {
-            return true;
-        }
     }
-    
+    return true;
+}*/
+[1,2,4,6]
 
-    return esPar(r);
+function laColaEsPar(lista) {
+    const [primerNumero, ...resto] = lista;
+
+    /*if(lista.length < 2){
+        return false;
+    }*/
+
+    if(resto.length == 0){
+        return false;
+    }
+
+    for(i=0;i<resto.length;i++){
+       if(!esPar(resto[i])){
+        return false;
+       } 
+    }
+    return true;
+
 }
 
 
